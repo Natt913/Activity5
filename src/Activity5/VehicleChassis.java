52ad81755d3c6ce8b2d9083e90f87613deb989a1
Natt913 <nathalie.crespo913@gmail.com>
2018-10-10 09:11:41 -0400

    InteriorFeature

--- src/Activity5/VehicleChassis.java
+++ src/Activity5/VehicleChassis.java
@@ -5,40 +5,41 @@ public class VehicleChassis implements Chassis {
   public String chassisName;
 
   //A public default constructor
-  //Set the chassisName instance variable in both, use the interface constant as the default String value.
-  public VehicleChassis(){
-  this.chassisName = chassis;
+  //Set the chassisName instance variable in both
+  // Use the interface constant as the default String value.
+  public VehicleChassis() {
+    this.chassisName = chassis;
   }
 
   //And an overloaded constructor with the following value:
   //A String with a parameter value of chassisName
   //Set the chassisName instance variable
-  public VehicleChassis(String chassisName){
+  public VehicleChassis(String chassisName) {
     this.chassisName = chassisName;
   }
 
-  // A public method named getChassisType that doesnt have a formal parameter and that returns an instance
+  // A public method named getChassisType that doesnt have a formal parameter
+  // And that returns an instance
   //of the interface Chassis (hint that would be a copy of this class).
-  public Chassis getChassisType(){
+  public Chassis getChassisType() {
     return this;
   }
 
-  // A public method setChassisType that takes a String parameter vehicleChassis and that returns a void. It
-  //should set the instance variable chassisName.
-  public void setChassisType(String vehicleChassis){
+  // A public method setChassisType that takes a String parameter vehicleChassis
+  // And that returns a void.
+  // It should set the instance variable chassisName.
+  public void setChassisType(String vehicleChassis) {
 
     chassisName = vehicleChassis;
   }
 
   // A public toString method that returns the following:
   //Chassis Name : Chassis
-  public String toString(){
+  public String toString() {
 
     return "Chassis Name : " + chassisName;
   }
 
-  //Write a static main method that creates two objects, one with the default constructor and the other with the
-  //constructor with parameters. Give sample data for the parameters
 
 
 
